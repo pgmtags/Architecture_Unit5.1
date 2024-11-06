2e50226f302f8b17280f436ce3315e8889a3bfb6
Andrij Zyniuk <62100293+pgmtags@users.noreply.github.com>
2024-11-06 18:47:40 +0200

    Revert "Fixed highlighting & shifting"
    This reverts commit 43c5afc2cf8e7abfbbccfd164ef51fd7555d87c4.

--- cmake-build-debug/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrijzyn/CLionProjects/untitled/Architecture_Unit5/1/cmake-build-debug/_deps/googletest-src/googletest
-# Build directory: /home/andrijzyn/CLionProjects/untitled/Architecture_Unit5/1/cmake-build-debug/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
